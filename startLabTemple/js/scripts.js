3a07a6a82efff407d492f0552453f9019757faa5
puurpllotus <longjohntabinye@gmail.com>
2026-01-30 15:08:22 +0100

    siteplan

---
+++ startLabTemple/js/scripts.js
@@ -0,0 +1,17 @@
+import { temples } from '../data/temples.js'
+console.log(temples);
+
+import { temples } from '../data/temples.js'
+console.log(url);
+
+const showHere = document.querySelector('#showHere');
+
+const mydialog = document.querySelector('#mydialog');
+const mytitle = document.querySelector('#mytitle');
+const myinfo = document.querySelector('#myinfo');
+const myclose = document.querySelector('#myclose');
+myclose.addEventListener('click', () => mydialog.close());
+
+function displayItem(data) {
+   
+}
